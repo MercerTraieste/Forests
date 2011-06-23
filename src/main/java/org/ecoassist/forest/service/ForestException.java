347d051c4e2e76ce6c6e4eccdb8b5ec27f4d1f25
Emilian Losneanu <emi@emi.local>
2011-06-23 15:35:27 +0200

    Cleanup

--- src/main/java/org/ecoassist/forest/service/ForestException.java
+++ src/main/java/org/ecoassist/forest/service/ForestException.java
@@ -1,13 +1,6 @@
 package org.ecoassist.forest.service;
 
 public class ForestException extends Exception {
-    public ForestException() {
-        super();
-    }
-
-    public ForestException(String s) {
-        super(s);
-    }
 
     public ForestException(String s, Throwable throwable) {
         super(s, throwable);
@@ -17,7 +10,4 @@ public class ForestException extends Exception {
         super(throwable);
     }
 
-    protected ForestException(String s, Throwable throwable, boolean b, boolean b1) {
-        super(s, throwable, b, b1);
-    }
 }
